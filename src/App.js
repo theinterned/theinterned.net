535021f78869a029e9dde2f6bbba974d7964ad21
Ned Schwartz <ned@theinterned.net>
2016-02-16 17:37:47 -0500

    Marked-up and styled complete first draft of the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import CSSModules from 'react-css-modules';
 import globalStyles from './global.scss';
+import styles from './App.scss';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <h1>Hi, I'm Ned.</h1>
+      <div className="app">
+        <h1 styleName='title'>Hi, I'm Ned</h1>
+        <div styleName="blurb">
+          <p>I’m a seasoned web developer and designer with over 15 years of
+            experience working on high profile, heavily-trafficked websites.</p>
+          <p>Most recently, I was the lead designer and front-end developer
+            on <a href="http://getcarta.com" className="carta">Carta</a>, a
+            cloud-based social inventory and order management app. I was
+            responsible for the design and development of Carta’s beautiful and
+            responsive interface. Carta is a rich and non-trivial business app
+            and I learned a great deal about balancing complexity and ease of
+            use.</p>
+          <p>I pride myself on writing beautiful, semantic code that is
+            cross-browser compatible and standards compliant. I specializes in
+            authoring rich user experiences with the latest web technologies.
+            I’m a trained artist and a strong UI and UX designer with an
+            interest in user-focused design practices. I’m creative, fastidious,
+            dedicated, ambitious and helpful. I enjoy problem solving,
+            collaboration and knowledge-sharing. I’m a quick study and eager
+            contributor.</p>
+          <p>Yes this website is very bare-bones: It's <a href="https://github.com/theinterned/theinterned.net">
+            under construction</a> like all of the web! I'm looking for work now
+            and wanted to get something up ASAP &mdash; <a href="mailto:ned@theinterned.net?subject=let%27s%20work%20together%20to%20make%20the%20internet%20great!">Hire me!</a></p>
+          <p styleName="CV">
+            <a href=""><span className="emoji">🌐</span> Here's my CV as a PDF</a>
+          </p>
+        </div>
+        <ul styleName="links">
+          <li styleName="link" className="dribbble"><a href="https://dribbble.com/theinterned">Dribbble</a></li>
+          <li styleName="link" className="github"><a href="https://github.com/theinterned">Github</a></li>
+          <li styleName="link" className="instapaper"><a href="https://www.instapaper.com/p/theinterned">Instapaper</a></li>
+          <li styleName="link" className="linkdin"><a href="https://ca.linkedin.com/in/neddo">Linkedin</a></li>
+        </ul>
+      </div>
     );
   }
 }
+
+export default CSSModules(App, styles);
