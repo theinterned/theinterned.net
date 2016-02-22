1d0a693b72ac605888a009b30057f55201ca92b8
Ned Schwartz <ned@theinterned.net>
2016-02-22 17:02:40 -0500

    the dev config for web pack isomorphic tools is working

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,12 +1,22 @@
 var path = require('path');
 var webpack = require('webpack');
+var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin')
 
-var rootPath = path.resolve(__dirname, '../'); // Relative path from this file to the root of the app
-var publicPath = '/static/'
+var webpackIsomorphicToolsPlugin =
+  // webpack-isomorphic-tools settings reside in a separate .js file
+  // (because they will be used in the web server code too).
+  new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools.config'))
+  // also enter development mode since it's a development webpack configuration
+  // (see below for explanation)
+  .development()
+
+var rootPath = path.resolve(__dirname, '..'); // Relative path from this file to the root of the app
+var assetPath = path.resolve(rootPath, '/static/');
 var projectPath = path.resolve(rootPath, './dist'); // => `./dist`
 
 module.exports = {
   devtool: 'eval',
+  context: path.resolve(__dirname, '..'),
   entry: [
     'webpack-dev-server/client?http://0.0.0.0:3000',
     'webpack/hot/only-dev-server',
@@ -15,6 +25,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: projectPath,
+    publicPath: assetPath
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
