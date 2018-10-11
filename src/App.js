86fd1ebab417c94b293d758eb5d98f2416c9c603
Ned Schwartz <ned@theinterned.net>
2018-10-11 19:14:22 -0400

    deltes the hidden blurb

--- src/App.js
+++ src/App.js
@@ -138,23 +138,6 @@ class App extends Component {
           &mdash;<br/>
           I design & build<br/>
           web apps.</Title>
-        <div className="blurb">
-          <p>I’m a seasoned web developer and designer with almost 20 years of
-            experience working on high profile, heavily-trafficked websites.</p>
-          <p>Most recently, I was the lead designer and front-end developer
-            on <a href="http://getcarta.com" className="carta">Carta</a>, a
-            cloud-based social inventory and order management app. I was
-            responsible for the design and development of Carta’s beautiful and
-            responsive interface. Carta is a rich and non-trivial business app
-            and I learned a great deal about balancing complexity and ease of
-            use.</p>
-          <p>Yes this website is very bare-bones: It's <a href="https://github.com/theinterned/theinterned.net">
-            under construction</a> like all of the web! I'm looking for work now
-            and wanted to get something up ASAP &mdash; <a href="mailto:ned@theinterned.net?subject=let%27s%20work%20together%20to%20make%20the%20internet%20great!">Hire me!</a></p>
-          <p className="CV_dl">
-            <a href="https://www.dropbox.com/s/3jxewz8yj4mg9tz/Ned_Schwartz_CV.pdf?dl=0">↬ Here's my CV as a PDF</a>
-          </p>
-        </div>
         <Links>
           <LinkItem><Link href="https://www.dropbox.com/s/3jxewz8yj4mg9tz/Ned_Schwartz_CV.pdf?dl=0">CV</Link></LinkItem>
           <LinkItem><Link href="https://dribbble.com/theinterned">Dribbble</Link></LinkItem>
